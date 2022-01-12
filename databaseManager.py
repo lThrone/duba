f63b66ddac1f3dc33eef07994eaab27d8e8420e2
Emre Önal <inf3085@hs-worms.de>
2022-01-12 18:10:58 +0100

    add delete query for database

--- databaseManager.py
+++ databaseManager.py
@@ -3,14 +3,17 @@ from pymongo import MongoClient
 client = MongoClient(port=27017)
 db=client["vocabularydatabase"]
 
+questionsLearnModule = []
+
 class DatabaseManager():
-    def __init__(self, id, deutsch, englisch, gelernt):
+    def __init__(self, id = 0, deutsch = "", englisch = "", gelernt = ""):
         self.id = id
         self.deutsch = deutsch
         self.englisch = englisch
         self.gelernt = gelernt
         self.dbColGerToEng = db["GermanToEnglish"]
         #self.dbColEngToGer = db["EnglishToGerman"]
+
 
 
     def createInstanceGerToEng(self):
@@ -24,8 +27,14 @@ class DatabaseManager():
 
     def getDocument(self):
         documents = self.dbColGerToEng.find()
+        if questionsLearnModule:
+            questionsLearnModule.clear()
         for x in documents:
+            questionsLearnModule.append(dict(x))
             print(x)
+        return questionsLearnModule
 
-        return documents
-
+    def deleteEntrys(self):
+        query = {"deutsch": {"$regex": "*"}} # remove filter
+        d = self.dbColGerToEng.delete_many({})
+        print(d.deleted_count, " documents deleted !!")
